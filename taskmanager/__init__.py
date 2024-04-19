05fda65889f304e94e7ad323bb28abf0cfb98cd5
Leon Potgieter <hexvallei@gmail.com>
2024-04-19 19:20:09 +0000

    bug psycopg version control needed update

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,6 +6,7 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
