5a7549efbf774b13025f0cbd27a704324691aba2
Leon Potgieter <hexvallei@gmail.com>
2024-04-20 13:36:51 +0000

    add edit function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
+    if (window.location.pathname == "/add_category") {
+      $('#category_name').focus()
+    }
+
   });
 
 let footerText = `<a class="grey-text text-lighten-4 right" href="https://github.com/leonp84" target="_blank">Leon A. Potgieter</a>`
